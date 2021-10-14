aed333689f6cdaccb60e55ee9dd833264a90515c
StrawHat <joaovictorSA972@gmail.com>
2021-10-14 15:01:39 -0300

    seila

--- db/schema.rb
+++ db/schema.rb
@@ -12,50 +12,49 @@
 
 ActiveRecord::Schema.define(version: 2019_06_06_170145) do
 
-    # These are extensions that must be enabled in order to support this database
-    enable_extension "plpgsql"
-  
-    create_table "appointments", force: :cascade do |t|
-      t.datetime "appointment_date"
-      t.time "start_time"
-      t.integer "status"
-      t.bigint "user_id"
-      t.bigint "room_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.string "description"
-      t.index ["room_id"], name: "index_appointments_on_room_id"
-      t.index ["user_id"], name: "index_appointments_on_user_id"
-    end
-  
-    create_table "rooms", force: :cascade do |t|
-      t.string "name"
-      t.string "location"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "students"
-    end
-  
-    create_table "users", force: :cascade do |t|
-      t.string "email", default: "", null: false
-      t.string "encrypted_password", default: "", null: false
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"
-      t.integer "sign_in_count", default: 0, null: false
-      t.datetime "current_sign_in_at"
-      t.datetime "last_sign_in_at"
-      t.inet "current_sign_in_ip"
-      t.inet "last_sign_in_ip"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.string "username"
-      t.string "registration"
-      t.string "course"
-      t.boolean "is_admin", default: false
-      t.index ["email"], name: "index_users_on_email", unique: true
-      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    end
-  
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_date"
+    t.time "start_time"
+    t.integer "status"
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.index ["room_id"], name: "index_appointments_on_room_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
-  
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "students"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "registration"
+    t.string "course"
+    t.boolean "is_admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
